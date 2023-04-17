7dd0cffe0e71327c4384efbcfa855a6e64e978fd
Airat <abdrakovairat@gmail.com>
2023-04-17 18:21:00 +0300

    selecteditor mb done

--- ViewModels/SelectEditorWindowViewModel.cs
+++ ViewModels/SelectEditorWindowViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using cumcad.Models.Other;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,17 +25,35 @@ namespace cumcad.ViewModels
         internal string ImagePath { get; set; }
     }
 
-    internal class SelectEditorWindowViewModel
+    internal class SelectEditorWindowViewModel : BindableBase
     {
         private SelectEditorResult result = new SelectEditorResult();
 
+        private int selectedTabIndex;
+        public int SelectedTabIndex
+        {
+            get { return selectedTabIndex; }
+            set { SetProperty(ref selectedTabIndex, value); }
+        }
+
+        private string imageFilePath;
+        public string ImageFilePath
+        {
+            get { return imageFilePath; }
+            set { SetProperty(ref imageFilePath, value); }
+        }
+
         #region Commands
+        public ICommand OpenFileCommand { get; set; }
+        public ICommand CreateEditorCommand { get; set; }
         public ICommand CloseWindowCommand { get; set; }
         #endregion
 
         internal SelectEditorWindowViewModel() 
         {
             CloseWindowCommand = new DelegateCommand(OnCloseWindowCommand);
+            CreateEditorCommand = new DelegateCommand(OnCreateWindowCommand);
+            OpenFileCommand = new DelegateCommand(OnOpenFileCommand);
         }
 
         async internal Task<SelectEditorResult> GetSelectionTask()
@@ -46,6 +66,39 @@ namespace cumcad.ViewModels
                 }
             });
             return result;
+        }
+
+        private void OnOpenFileCommand(object paramenter)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Title = "Browse Image File",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "png",
+                Filter = "png files (*.png)|*.png",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if ((bool)openFileDialog.ShowDialog())
+            {
+                string selectedFile = openFileDialog.FileName;
+
+                ImageFilePath = selectedFile;
+            }
+        }
+
+        private void OnCreateWindowCommand(object paramenter)
+        {
+            result.IsSelected = true;
+            result.ImagePath = ImageFilePath;
+            (paramenter as Window).Close();
         }
 
         private void OnCloseWindowCommand(object paramenter)
--- Views/SelectEditorWindowView.xaml.cs
+++ Views/SelectEditorWindowView.xaml.cs
@@ -1,22 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace cumcad.Views
 {
-    /// <summary>
-    /// Interaction logic for SelectEditorWindow.xaml
-    /// </summary>
+    public class EditorTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (((int)value).ToString() == (string)parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (bool)value ? parameter : null;
+        }
+    }
+
     public partial class SelectEditorWindowView : Window
     {
         public SelectEditorWindowView()
