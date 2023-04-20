0dcb4608c3e940c3bcd3bb2f4289f89873bbaeab
Airat <abdrakovairat@gmail.com>
2023-04-20 14:20:36 +0300

    rangeslider added and inrange visual done

--- ViewModels/Handlers/InRangeViewModel.cs
+++ ViewModels/Handlers/InRangeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using cumcad.ViewModels.Base;
+﻿using cumcad.Models.Classes;
+using cumcad.ViewModels.Base;
 using OpenCvSharp;
 using Prism.Mvvm;
 using System;
@@ -11,6 +12,48 @@ namespace cumcad.ViewModels.Handlers
 {
     internal class InRangeViewModel : BindableBase, IHandler
     {
+        private int redLowerValue;
+        public int RedLowerValue
+        {
+            get { return redLowerValue; }
+            set { SetProperty(ref redLowerValue, value); }
+        }
+
+        private int redHigherValue;
+        public int RedHigherValue
+        {
+            get { return redHigherValue; }
+            set { SetProperty(ref redHigherValue, value); }
+        }
+
+        private int greenLowerValue;
+        public int GreenLowerValue
+        {
+            get { return greenLowerValue; }
+            set { SetProperty(ref greenLowerValue, value); }
+        }
+
+        private int greenHigherValue;
+        public int GreenHigherValue
+        {
+            get { return greenHigherValue; }
+            set { SetProperty(ref greenHigherValue, value); }
+        }
+
+        private int blueLowerValue;
+        public int BlueLowerValue
+        {
+            get { return blueLowerValue; }
+            set { SetProperty(ref blueLowerValue, value); }
+        }
+
+        private int blueHigherValue;
+        public int BlueHigherValue
+        {
+            get { return blueHigherValue; }
+            set { SetProperty(ref blueHigherValue, value); }
+        }
+
         public List<Mat> GetResult(List<Mat> images)
         {
             throw new NotImplementedException();
