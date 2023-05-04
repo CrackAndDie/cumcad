9b13e2c30ddbb3958e5d56a1938c48a304a2c9f1
Airat <abdrakovairat@gmail.com>
2023-05-04 13:25:35 +0300

    HandlerFactory changed

--- Models/Factories/HandlerFactory.cs
+++ Models/Factories/HandlerFactory.cs
@@ -14,97 +14,27 @@ namespace cumcad.Models.Factories
 {
     internal class HandlerFactory
     {
-        internal static List<string> StringItems = new List<string>() { "ExtractImage", "InRange", "InRangeGray", "Resize", "RejectBorders", "CvtColor", "CountNonZero", "BitwiseNot", "BitwiseOr", "Anime" };
+        // viewed list
+        internal static List<string> StringItems = new List<string>() { "ExtractImage", "InRange", "InRangeGray", "Resize", "RejectBorders", "CvtColor", "CountNonZero", "BitwiseNot", "BitwiseOr" };
+        // real names
+        internal static List<string> StringItemsRealNames = new List<string>() { "ExtractImage", "InRange", "InRangeGray", "Resize", "RejectBorders", "CvtColor", "CountNonZero", "BitwiseNot", "BitwiseOr" };
 
         internal static UserControl GetHandler(string name)
         {
             try
             {
-                Type type = typeof(HandlerFactory);
-                MethodInfo methodInfo = type.GetMethod("Get" + name, BindingFlags.NonPublic | BindingFlags.Static);
-                if (methodInfo != null)
-                {
-                    return methodInfo.Invoke(name, null) as UserControl;
-                }
+                string realName = StringItemsRealNames[StringItems.IndexOf(name)];
+                var viewType = Assembly.GetExecutingAssembly().GetType("cumcad.Views.Handlers." + realName + "View");
+                var view = (UserControl)Activator.CreateInstance(viewType);
+                var vmType = Assembly.GetExecutingAssembly().GetType("cumcad.ViewModels.Handlers." + realName + "ViewModel");
+                view.DataContext = Activator.CreateInstance(vmType);
+                return view;
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                // there is no that method....
-                Debug.WriteLine(ex);
+                // idk if there could be an error
             }
             return null;
-        }
-
-        private static UserControl GetExtractImage()
-        {
-            var view = new ExtractImageView();
-            var vm = new ExtractImageViewModel();
-            view.DataContext = vm;
-            return view;
-        }
-
-        private static UserControl GetInRange()
-        {
-            var view = new InRangeView();
-            var vm = new InRangeViewModel();
-            view.DataContext = vm;
-            return view;
-        }
-
-        private static UserControl GetInRangeGray()
-        {
-            var view = new InRangeGrayView();
-            var vm = new InRangeGrayViewModel();
-            view.DataContext = vm;
-            return view;
-        }
-
-        private static UserControl GetResize()
-        {
-            var view = new ResizeView();
-            var vm = new ResizeViewModel();
-            view.DataContext = vm;
-            return view;
-        }
-
-        private static UserControl GetRejectBorders()
-        {
-            var view = new RejectBordersView();
-            var vm = new RejectBordersViewModel();
-            view.DataContext = vm;
-            return view;
-        }
-
-        private static UserControl GetCvtColor()
-        {
-            var view = new CvtColorView();
-            var vm = new CvtColorViewModel();
-            view.DataContext = vm;
-            return view;
-        }
-
-        private static UserControl GetCountNonZero()
-        {
-            var view = new CountNonZeroView();
-            var vm = new CountNonZeroViewModel();
-            view.DataContext = vm;
-            return view;
-        }
-
-        private static UserControl GetBitwiseNot()
-        {
-            var view = new BitwiseNotView();
-            var vm = new BitwiseNotViewModel();
-            view.DataContext = vm;
-            return view;
-        }
-
-        private static UserControl GetBitwiseOr()
-        {
-            var view = new BitwiseOrView();
-            var vm = new BitwiseOrViewModel();
-            view.DataContext = vm;
-            return view;
         }
     }
 }
